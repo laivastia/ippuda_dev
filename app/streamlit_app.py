61f474fe70ae1d8fe6b3066895a3f7205b550cb9
LeoHuni <68592195+LeoHuni@users.noreply.github.com>
2023-05-15 17:35:56 +0900

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -44,7 +44,7 @@ def init_connection():
     except Exception as e:
         print(e)
     return db
-@st.cache_resource
+# @st.cache_resource
 # Define a function to reset the database
 def reset_db():
     collection_name = ['src','alligned','w_line','wo_line']
