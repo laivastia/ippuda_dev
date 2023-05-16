b3f87035365ce91a877d0be9ef851ca3a6c6c9a0
LeoHuni <68592195+LeoHuni@users.noreply.github.com>
2023-05-16 14:34:06 +0900

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -37,13 +37,14 @@ def init_connection():
         client.admin.command('ping')
         db = client["Ippuda"]
         print("Pinged your deployment. You successfully connected to MongoDB!")
+        
     except Exception as e:
         print(e)
     return db
 # @st.cache_resource
 # def init_connection():
 #     return MongoClient(**st.secrets["mongo"])
-@st.cache_resource
+# @st.cache_resource
 # Define a function to reset the database
 def reset_db():
     collection_name = ['src','alligned','w_line','wo_line']
