c10fe7d94d974c31b091a11aaf8218eb39b64e0d
LeoHuni <68592195+LeoHuni@users.noreply.github.com>
2023-05-16 14:39:34 +0900

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -44,7 +44,7 @@ def init_connection():
 # @st.cache_resource
 # def init_connection():
 #     return MongoClient(**st.secrets["mongo"])
-# @st.cache_resource
+@st.cache
 # Define a function to reset the database
 def reset_db():
     collection_name = ['src','alligned','w_line','wo_line']
