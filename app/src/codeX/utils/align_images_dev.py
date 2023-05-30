c81f8dfd27c1dea5bdb1f64881a7ecb7022150b4
vastia <92559779+laivastia@users.noreply.github.com>
2023-05-30 18:06:12 +0900

    Update align_images_dev.py

--- app/src/codeX/utils/align_images_dev.py
+++ app/src/codeX/utils/align_images_dev.py
@@ -128,31 +128,27 @@ def unpack_bz2(src_path):
     return dst_path
 
 def align_images_dev(my_image, target_image):
-    if __name__ == "__main__":
-        """
-        Extracts and aligns all faces from images using DLib and a function from original FFHQ dataset preparation step
-        python align_images.py /raw_images /aligned_images
-        """
-        parser = argparse.ArgumentParser(description='Align faces from input images', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        # parser.add_argument('raw_dir', default = r'code\utils\images', help='Directory with raw images for face alignment')
-        # parser.add_argument('aligned_dir', default = r'code\utils\images\aligned_images',help='Directory for storing aligned images')
-        parser.add_argument('--output_size', default=512, help='The dimension of images for input to the model', type=int)
-        parser.add_argument('--x_scale', default=1, help='Scaling factor for x dimension', type=float)
-        parser.add_argument('--y_scale', default=1, help='Scaling factor for y dimension', type=float)
-        parser.add_argument('--em_scale', default=0.1, help='Scaling factor for eye-mouth distance', type=float)
-        parser.add_argument('--use_alpha', default=False, help='Add an alpha channel for masking', type=bool)
+    """
+    Extracts and aligns all faces from images using DLib and a function from original FFHQ dataset preparation step
+    python align_images.py /raw_images /aligned_images
+    """
+    parser = argparse.ArgumentParser(description='Align faces from input images', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # parser.add_argument('raw_dir', default = r'code\utils\images', help='Directory with raw images for face alignment')
+    # parser.add_argument('aligned_dir', default = r'code\utils\images\aligned_images',help='Directory for storing aligned images')
+    parser.add_argument('--output_size', default=512, help='The dimension of images for input to the model', type=int)
+    parser.add_argument('--x_scale', default=1, help='Scaling factor for x dimension', type=float)
+    parser.add_argument('--y_scale', default=1, help='Scaling factor for y dimension', type=float)
+    parser.add_argument('--em_scale', default=0.1, help='Scaling factor for eye-mouth distance', type=float)
+    parser.add_argument('--use_alpha', default=False, help='Add an alpha channel for masking', type=bool)
 
-        args, other_args = parser.parse_known_args()
+    args, other_args = parser.parse_known_args()
 
-        # 내이미지와 타겟 이미지 두개 numpy.arr = RAW_IMAGES
-        ALIGNED_IMAGES_DIR = r'images\aligned_images' #args.aligned_dir
+    # 내이미지와 타겟 이미지 두개 numpy.arr = RAW_IMAGES
+    ALIGNED_IMAGES_DIR = r'images\aligned_images' #args.aligned_dir
 
-        landmarks_detector = LandmarksDetector()
-        print('Aligning ...')
-        try:
-            print('Getting landmarks...')
-            myimage = [image_align(my_image, face_landmarks, output_size=args.output_size, x_scale=args.x_scale, y_scale=args.y_scale, em_scale=args.em_scale, alpha=args.use_alpha) for i, face_landmarks in enumerate(landmarks_detector.get_landmarks(my_image), start=1)]
-            targetimg = [image_align(target_image, face_landmarks, output_size=args.output_size, x_scale=args.x_scale, y_scale=args.y_scale, em_scale=args.em_scale, alpha=args.use_alpha) for i, face_landmarks in enumerate(landmarks_detector.get_landmarks(target_image), start=1)]
-            return [myimage, targetimg]
-        except:
-            print("Exception in landmark detection!")
+    landmarks_detector = LandmarksDetector()
+
+    myimage = [image_align(my_image, face_landmarks, output_size=args.output_size, x_scale=args.x_scale, y_scale=args.y_scale, em_scale=args.em_scale, alpha=args.use_alpha) for i, face_landmarks in enumerate(landmarks_detector.get_landmarks(my_image), start=1)]
+    targetimg = [image_align(target_image, face_landmarks, output_size=args.output_size, x_scale=args.x_scale, y_scale=args.y_scale, em_scale=args.em_scale, alpha=args.use_alpha) for i, face_landmarks in enumerate(landmarks_detector.get_landmarks(target_image), start=1)]
+    return [myimage, targetimg]
+
